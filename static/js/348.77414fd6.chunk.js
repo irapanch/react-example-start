50e1556cf49d317dfa75e8eaa21727260be4c033
irapanch <117355580+irapanch@users.noreply.github.com>
2024-03-21 18:28:10 +0000

    Deploying to gh-pages from @ irapanch/react-example-start@dceef26c90dce5d3c42a8b03aedf75806a0e0082 🚀

---
+++ static/js/348.77414fd6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_example_start=self.webpackChunkreact_example_start||[]).push([[348],{348:function(e,t,r){r.r(t);r(791);var n=r(689),s=r(297),a=r(717),i=r(184);t.default=function(){var e=(0,n.UO)().postId,t=(0,s.i)(a.w8,e).data,r=void 0===t?[]:t;return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)("h2",{children:"Comments by postID"}),(0,i.jsx)("hr",{}),(0,i.jsx)("ul",{children:null===r||void 0===r?void 0:r.map((function(e){return(0,i.jsx)("li",{children:e.body},e.id)}))})]})}}}]);
+//# sourceMappingURL=348.77414fd6.chunk.js.map
